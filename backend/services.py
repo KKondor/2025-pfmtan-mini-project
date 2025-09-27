03bd328619384f2a1508180ccf555ce514a71313
Bence Sebestyén <bence.sebestyen@mailbox.unideb.hu>
2025-09-28 01:11:52 +0200

    feat: implement generate_joke() method in JokeService

--- backend/services.py
+++ backend/services.py
@@ -1,5 +1,14 @@
 from repository import Joke, JokeRepository
+import random
 
 class JokeService:
     def __init__(self, repository: JokeRepository):
         self.repository = repository
+
+    def generate_joke(self) -> Joke:
+        random_id = random.randint(1, 600)
+        joke = self.repository.get_joke_by_id(random_id)
+        if joke:
+            return joke
+        else:
+            raise ValueError("No joke found with the generated ID.")
