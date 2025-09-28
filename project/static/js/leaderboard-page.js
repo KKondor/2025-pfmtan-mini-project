c9190bbd04e54a9400624bc3b90759df1a8f5959
Lacaa3 <laci.nyiri04@gmail.com>
2025-09-28 20:08:12 +0200

    feat: add leaderboard DOM reference and fetch call

---
+++ project/static/js/leaderboard-page.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const leaderboardList = document.getElementById("leaderboard-list");
+
+    try {
+        const response = await fetch("/leaderboard");
+        if (!response.ok) throw new Error("Failed to fetch leaderboard");
+
+        const jokes = await response.json();
+
+    }
+  });
