981181724ec35ab34005f6dc441bb86a1594a9c1
Bence Sebestyén <bence.sebestyen@mailbox.unideb.hu>
2025-09-29 01:55:03 +0200

    test: implement test_get_joke method for testing GET /joke endpoint

--- project/tests/test_app.py
+++ project/tests/test_app.py
@@ -5,3 +5,13 @@ from project import app
 def client():
     with app.test_client() as client: # create test client
         yield client
+
+# GET /joke endpoint test
+def test_get_joke(client):
+    response = client.get('/joke')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert 'id' in data
+    assert 'setup' in data
+    assert 'punchline' in data
+    assert 'rating' in data
