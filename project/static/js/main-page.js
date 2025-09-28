f49ccebdc9508b6d2e094f35b1bd766981c2714d
Lacaa3 <laci.nyiri04@gmail.com>
2025-09-28 19:21:42 +0200

    feat(main-page.js): initialize DOM elements

---
+++ project/static/js/main-page.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const jokeBtn = document.getElementById("joke-btn");
+    const setupEl = document.getElementById("setup");
+    const punchlineEl = document.getElementById("punchline");
+    const likeBtn = document.querySelector(".like-btn");
+    const dislikeBtn = document.querySelector(".dislike-btn");
+    const buttonsDiv = document.querySelector(".buttons");
