c8d01a535b07a4b4892bab6f99b50afaa16db4e9
Bence Sebestyén <bence.sebestyen@mailbox.unideb.hu>
2025-09-28 03:27:47 +0200

    feat: integrate main- and leaderboard pages with routes

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from services import JokeService
+from repository import JokeRepository
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="../src/pages",
+    static_folder="../src/pages/"
+)
 
-service = JokeService()
+repo = JokeRepository()
+service = JokeService(repo)
+
+@app.route('/')
+def main_page():
+    return render_template('main-page/main-page.html')
+
+@app.route('/leaderboard-page')
+def leaderboard_page():
+    return render_template('leaderboard-page/leaderboard-page.html')
 
 @app.route('/joke', methods=['GET'])
 def get_joke():
@@ -20,3 +34,6 @@ def get_leaderboard():
 @app.route('/joke/<int:id>/rate', methods=['POST'])
 def rate_joke(id):
     pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
