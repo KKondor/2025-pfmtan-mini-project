11d687eafcdae191633bb47dbbcaf169b172fe77
Bence Sebestyén <bence.sebestyen@mailbox.unideb.hu>
2025-09-29 02:39:10 +0200

    test: implement test_main_page to check if it loads

--- project/tests/test_app.py
+++ project/tests/test_app.py
@@ -51,3 +51,7 @@ def test_rate_joke_invalid_value(client):
     response = client.post('/joke/1/rate', json={'value': 0})
     assert response.status_code == 500
 
+# test if main-page.html loads
+def test_main_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
